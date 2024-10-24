7c65f70a0f044026bcede52b3ef91ec9d105df10
Newt <guoching130@gmail.com>
2024-10-24 21:15:16 +0800

    main

--- .config/nvim/lua/config/autocmds.lua
+++ .config/nvim/lua/config/autocmds.lua
@@ -5,6 +5,6 @@
 vim.api.nvim_create_autocmd("BufWritePre", {
   pattern = "*.ak",
   callback = function()
-    vim.lsp.buf.format({async = true})
+    vim.lsp.buf.format({async = false})
   end
 })

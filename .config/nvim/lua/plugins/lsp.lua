5960b289d34fc90cc9dc9fc4f4f19995d4c5eb68
Newt6611 <guoching130@gmail.com>
2025-12-01 21:43:03 +0800

    fea

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -8,12 +8,12 @@ return {
             ['*'] = {
                 keys = {
                     { "rn", "<cmd>lua vim.lsp.buf.rename()<CR>", has = "definition"},
-                    { "gd", "<cmd>lua vim.lsp.buf.definition()<CR>", has = "definition"},
+                    { "gd", "<cmd>Trouble lsp_definitions<CR>", has = "definition"},
                     { "<C-m>", "<cmd>lua vim.lsp.buf.code_action()<CR>", has = "definition"},
                     { "<leader>f", "<cmd>lua vim.lsp.buf.implementation()<CR>", has = "definition"},
                     { "gt", "<cmd>lua vim.lsp.buf.type_definition()<CR>", has = "definition"},
-                    -- { "gr", "<cmd>Telescope lsp_references<CR>", has = "definition"},
-                    { "gr", "<cmd>Trouble lsp_references<CR>", has = "definition"},
+                    { "gr", "<cmd>Telescope lsp_references<CR>", has = "definition"},
+                    -- { "gr", "<cmd>Trouble lsp_references<CR>", has = "definition"},
                 },
             },
         },

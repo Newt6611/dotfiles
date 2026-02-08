1062c8df0353bc9b56546ccf16110a307d8b7fae
Newt6611 <guoching130@gmail.com>
2026-02-08 15:42:04 +0800

    sfd

--- .config/nvim/lua/configs/keymap.lua
+++ .config/nvim/lua/configs/keymap.lua
@@ -15,3 +15,23 @@ map("n", "n", "nzzzv")
 map("n", "N", "Nzzzv")
 
 map("n", "<space>e", vim.diagnostic.open_float, { noremap = true, silent = true, buffer = bufnr })
+
+map('n', '<leader>em', function()
+  -- Open a new empty buffer
+  vim.cmd('new')
+
+  -- Set buffer type to 'nofile' so it won't be associated with a file
+  vim.bo.buftype = 'nofile'
+
+  -- Hide the buffer when abandoned instead of closing it
+  vim.bo.bufhidden = 'hide'
+
+  -- Disable swapfile for this buffer
+  vim.bo.swapfile = false
+
+  -- Fill the buffer with the output of :messages, split by line
+  vim.api.nvim_buf_set_lines(0, 0, -1, false, vim.split(vim.fn.execute('messages'), '\n'))
+
+  -- Move the cursor to the last line of the buffer
+  vim.cmd('normal! G')
+end, { desc = 'Open message history' })

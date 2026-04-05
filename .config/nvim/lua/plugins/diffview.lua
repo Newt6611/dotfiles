a0423f639ebac88a63c7a56eceb23f12a58bb91d
Newt6611 <guoching130@gmail.com>
2026-04-05 21:16:31 +0700

    dfs

---
+++ .config/nvim/lua/plugins/diffview.lua
@@ -0,0 +1,13 @@
+return {
+  'sindrets/diffview.nvim',
+  lazy = true,
+  keys = {
+    {
+      'df',
+      mode = 'n',
+      '<CMD>DiffviewOpen<CR>',
+      desc = 'Open Diffview',
+    },
+  }
+}
+
--- .config/nvim/lua/plugins/gitsigns.lua
+++ .config/nvim/lua/plugins/gitsigns.lua
@@ -1,7 +1,12 @@
 return {
   'lewis6991/gitsigns.nvim',
   lazy = false,
-  opts = {},
+  opts = {
+    current_line_blame = true,
+    current_line_blame_opts = {
+      delay = 0,
+    },
+  },
   keys = {
     {
       '<leader>r',

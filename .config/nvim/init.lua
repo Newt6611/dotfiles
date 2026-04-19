6e16acd34f30204aa89ff687b9984d8bde59ba8a
Newt6611 <guoching130@gmail.com>
2026-04-20 00:55:22 +0800

    sdf

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -2,3 +2,5 @@ require('configs.autocmds')
 require('configs.options')
 require('configs.keymaps')
 require('configs.lazy')
+
+vim.cmd[[colorscheme tokyonight-night]]
--- .config/nvim/lua/plugins/theme.lua
+++ .config/nvim/lua/plugins/theme.lua
@@ -1,10 +1,6 @@
 return {
-  {
-    'catppuccin/nvim',
-    name = 'catppuccin',
-    priority = 1000,
-    config = function()
-      vim.cmd.colorscheme('catppuccin-mocha')
-    end,
-  },
+  "folke/tokyonight.nvim",
+  lazy = false,
+  priority = 1000,
+  opts = { },
 }
